accd38e2867941aa8662703ef1a4578ec9982fbc
Patrick Grubauer <40146720180276@litec.ac.at>
2022-07-07 18:35:10 +0200

    made an overall structure draft and added my currentProgress

---
+++ sandbox/rfidapp/lib/config/palette.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class ColorSelect {
+  static Color greenAccent = const Color.fromARGB(255, 1, 216, 89);
+}
---
+++ sandbox/rfidapp/lib/provider/card_repository/card_repository.dart
@@ -0,0 +1,4 @@
+//requests to get information from card repo
+import 'package:flutter/material.dart';
+
+class ClassName {}

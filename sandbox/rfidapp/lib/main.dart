dd47f052ff0b98a72d823805e821a7f27fee0f08
Patrick Grubauer <40146720180276@litec.ac.at>
2022-07-08 11:00:52 +0200

    gp

--- sandbox/rfidapp/lib/main.dart
+++ sandbox/rfidapp/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:rfidapp/pages/Login/login_page.dart';
 
 void main() {
-  runApp(new MaterialApp(
+  runApp(MaterialApp(
       theme: ThemeData(
         colorScheme: ColorScheme.fromSwatch().copyWith(
           primary: const Color.fromARGB(255, 1, 216, 89),
@@ -12,7 +12,7 @@ void main() {
       darkTheme:
           ThemeData(brightness: Brightness.dark, primarySwatch: Colors.green),
       themeMode: ThemeMode.system,
-      home: new LoginScreen(
+      home: const LoginScreen(
         title: 'Login Rfid',
       )));
 }

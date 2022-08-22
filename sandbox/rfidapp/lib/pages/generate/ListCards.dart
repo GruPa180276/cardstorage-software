b43c7ebb7e71d5fad0419f10d88977d36f4035f6
Patrick Grubauer <40146720180276@litec.ac.at>
2022-08-22 22:12:13 +0200

    some changes

--- sandbox/rfidapp/lib/pages/generate/ListCards.dart
+++ sandbox/rfidapp/lib/pages/generate/ListCards.dart
@@ -44,44 +44,60 @@ class ListCards {
             borderRadius: BorderRadius.circular(15.0),
           ),
           margin: const EdgeInsets.fromLTRB(15, 0, 15, 10),
-          child: Row(
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 12.0),
-                child: Icon(Icons.card_giftcard_sharp, size: 30),
-              ),
-              Container(
-                child: Column(
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 12.0),
+                  child: Icon(Icons.card_giftcard_sharp, size: 30),
+                ),
+                Column(
                   children: [
                     title,
                     Row(
                       children: [
-                        FlatButton(
-                            padding: EdgeInsets.symmetric(horizontal: 35),
-                            shape: Border(
-                                top: BorderSide(
-                                    color: ColorSelect.greyBorderColor),
-                                right: BorderSide(
-                                    color: ColorSelect.greyBorderColor)),
-                            color: Colors.transparent,
-                            splashColor: Colors.black,
-                            onPressed: _displayTextInputDialog,
-                            child: Text('Reservieren')),
-                        FlatButton(
-                            padding: EdgeInsets.symmetric(horizontal: 35),
-                            color: Colors.transparent,
-                            shape: Border(
-                                top: BorderSide(
-                                    color: ColorSelect.greyBorderColor)),
-                            //TODO ask if card is available and then ask if current time is not between reservated time
-                            onPressed: buildNfcScanner,
-                            child: Text('Jetzt holen'))
+                        ButtonBar(
+                          children: [
+                            FlatButton(
+                              padding: EdgeInsets.symmetric(horizontal: 30),
+                              shape: Border(
+                                  top: BorderSide(
+                                      color: ColorSelect.greyBorderColor),
+                                  right: BorderSide(
+                                      color: ColorSelect.greyBorderColor)),
+                              color: Colors.transparent,
+                              splashColor: Colors.black,
+                              onPressed: _displayTextInputDialog,
+                              child: Text('Reservieren',
+                                  style: TextStyle(
+                                      color: Theme.of(context).primaryColor)),
+                            ),
+                            FlatButton(
+                                padding: EdgeInsets.symmetric(horizontal: 30),
+                                // NEW,
+                                shape: Border(
+                                    top: BorderSide(
+                                        color: ColorSelect.greyBorderColor)),
+                                //TODO ask if card is available and then ask if current time is not between reservated time
+                                onPressed: buildNfcScanner,
+                                child: ConstrainedBox(
+                                  constraints: const BoxConstraints(
+                                      maxWidth: double.infinity),
+                                  child: Text('Jetzt holen',
+                                      style: TextStyle(
+                                          color:
+                                              Theme.of(context).primaryColor)),
+                                ))
+                          ],
+                        )
+                        //TODO chahnge to buttonbar
                       ],
                     )
                   ],
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         );
       });
@@ -97,7 +113,22 @@ class ListCards {
                     buildContext!, "Von:", vonTextEdidtingcontroller),
                 SizedBox(height: 20),
                 buildTimeChooseSection(
-                    buildContext!, "Bis:", bisTextEdidtingcontroller)
+                    buildContext!, "Bis:", bisTextEdidtingcontroller),
+                SizedBox(height: 20),
+                ElevatedButton(
+                    onPressed: () => print('tbc'),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 50.0, vertical: 15),
+                      child: Container(
+                        child: Text(
+                          'Jetzt Reservieren',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ))
               ],
             )
           ]);

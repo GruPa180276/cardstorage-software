5bc9a8935708cadcfe91d9e2d4bed65441d45d29
Patrick Grubauer <40146720180276@litec.ac.at>
2022-08-22 11:55:47 +0200

    added reservate system

--- sandbox/rfidapp/lib/config/palette.dart
+++ sandbox/rfidapp/lib/config/palette.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class ColorSelect {
   static Color greenAccent = const Color.fromARGB(255, 1, 216, 89);
   static Color blueAccent = const Color.fromARGB(255, 1, 49, 92);
+  static Color greyBorderColor = Colors.black12;
 
   //static Color blueAccent = const Color.fromRGBO(1, 49, 92, 97);
   //static Color blueAccent = const Color.fromRGBO(1, 49, 92, 97);

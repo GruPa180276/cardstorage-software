359cb1da56ec8ceb34b95f498a5bedf504fc7acc
Patrick Grubauer <40146720180276@litec.ac.at>
2022-08-30 15:54:43 +0200

    added email bot and password reset page

---
+++ sandbox/FlutterTesting/notificationnnest/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:notificationnnest/notifications.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatefulWidget {
+  @override
+  // ignore: library_private_types_in_public_api
+  _State createState() => _State();
+}
+
+class _State extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter - tutorisalskart.com'),
+        ),
+        body: Column(
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  NotificationApi.showNotification(
+                      title: 'Sarah Abs',
+                      body: 'I am simple not there',
+                      payload: 'sarah.abs');
+                },
+                child: Text('Simple Notification')),
+            ElevatedButton(
+                onPressed: () {}, child: Text('Scheduled Notification')),
+            ElevatedButton(onPressed: () {}, child: Text('Remove Notification'))
+          ],
+        ));
+  }
+}

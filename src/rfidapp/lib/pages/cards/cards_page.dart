93598c023e0ff6710d768393f55d09e98e23940c
Patrick Grubauer <40146720180276@litec.ac.at>
2022-09-01 11:03:57 +0200

    added searchbar refactoring

--- src/rfidapp/lib/pages/cards/cards_page.dart
+++ src/rfidapp/lib/pages/cards/cards_page.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:rfidapp/pages/generate/api_data_visualize.dart';
-import 'package:rfidapp/provider/types/cards.dart';
-import 'dart:async';
-import 'dart:convert';
-
-import 'package:rfidapp/provider/restApi/data.dart';
 
 class CardPage extends StatefulWidget {
   const CardPage({Key? key}) : super(key: key);
@@ -14,68 +9,8 @@ class CardPage extends StatefulWidget {
 }
 
 class _CardPage extends State<CardPage> {
-  Future<List<Cards>>? listOfCards;
-  String searchString = "";
-  TextEditingController searchController = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-    reloadCardList();
-  }
-
-  void reloadCardList() {
-    setState(() {
-      listOfCards = Data.getData("card").then((value) =>
-          jsonDecode(value.body).map<Cards>(Cards.fromJson).toList());
-      ApiVisualizer.listOfTypes = listOfCards;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          toolbarHeight: 100,
-          bottomOpacity: 0.0,
-          elevation: 0.0,
-          backgroundColor: Colors.transparent,
-          title: Text('Karten',
-              style: TextStyle(
-                  fontSize: 42,
-                  fontWeight: FontWeight.bold,
-                  color: Theme.of(context).primaryColor))),
-      body: Center(
-          child: Column(
-        children: [
-          TextField(
-              controller: searchController,
-              decoration: InputDecoration(
-                  prefixIcon: const Icon(Icons.search),
-                  hintText: 'Search Card',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide:
-                          BorderSide(color: Theme.of(context).dividerColor))),
-              onChanged: ((value) {
-                setState(() {
-                  ApiVisualizer.searchString = value;
-                });
-              })),
-          SizedBox(
-            height: 10,
-          ),
-          ApiVisualizer.build(context, "cards"),
-        ],
-      )),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Theme.of(context).secondaryHeaderColor,
-        child: const Icon(
-          Icons.replay,
-          color: Colors.white,
-        ),
-        onPressed: () => {reloadCardList()},
-      ),
-    );
+    return ApiVisualizer(site: 'Karten');
   }
 }
--- src/rfidapp/lib/pages/reservate/reservate_page.dart
+++ src/rfidapp/lib/pages/reservate/reservate_page.dart
@@ -1,59 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:rfidapp/pages/generate/api_data_visualize.dart';
-import 'package:rfidapp/provider/types/cards.dart';
-import 'dart:async';
-import 'dart:convert';
-
-import 'package:rfidapp/provider/restApi/data.dart';
 
 class ReservatePage extends StatefulWidget {
   const ReservatePage({Key? key}) : super(key: key);
 
   @override
-  State<ReservatePage> createState() => _ReservatePageState();
+  State<ReservatePage> createState() => _ReservatePage();
 }
 
-class _ReservatePageState extends State<ReservatePage> {
-  Future<List<Cards>>? listOfCards;
-
-  @override
-  void initState() {
-    super.initState();
-    reloadCardList();
-  }
-
-  void reloadCardList() {
-    setState(() {
-      listOfCards = Data.getData("card").then((value) =>
-          jsonDecode(value.body).map<Cards>(Cards.fromJson).toList());
-      ApiVisualizer.listOfTypes = listOfCards;
-    });
-  }
-
+class _ReservatePage extends State<ReservatePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 125,
-        bottomOpacity: 0.0,
-        elevation: 0.0,
-        backgroundColor: Colors.transparent,
-        title: Text('Ihre Reservierungen',
-            style: TextStyle(
-                fontFamily: 'Kanit',
-                fontSize: 35,
-                fontWeight: FontWeight.w900,
-                color: Theme.of(context).primaryColor)),
-      ),
-      body: Center(child: ApiVisualizer.build(context, "reservation")),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Theme.of(context).secondaryHeaderColor,
-        child: const Icon(
-          Icons.replay,
-          color: Colors.white,
-        ),
-        onPressed: () => {reloadCardList()},
-      ),
+    return ApiVisualizer(
+      site: 'Reservierungen',
     );
   }
 }

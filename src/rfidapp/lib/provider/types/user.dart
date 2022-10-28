16726811b6c28f526c95840f8ca6bad4baf5d612
Patrick Grubauer <40146720180276@litec.ac.at>
2022-10-28 15:10:46 +0200

    created json file for user

--- src/rfidapp/lib/provider/types/user.dart
+++ src/rfidapp/lib/provider/types/user.dart
@@ -1,12 +1,64 @@
-class Users {
-  final int userId;
+class User {
+  String? context;
+  String? businessPhones;
+  String? displayName;
+  String? givenName;
+  String? jobTitle;
+  String? mail;
+  String? mobilePhone;
+  String? officeLocation;
+  String? preferredLanguage;
+  String? surname;
+  String? userPrincipalName;
+  String? id;
 
   @override
-  const Users({
-    required this.userId,
-  });
+  User(
+      {this.context,
+      this.businessPhones,
+      this.displayName,
+      this.givenName,
+      this.jobTitle,
+      this.mail,
+      this.mobilePhone,
+      this.officeLocation,
+      this.preferredLanguage,
+      this.surname,
+      this.userPrincipalName,
+      this.id});
 
-  Users fromJson(json) => Users(
-        userId: json['userId'],
+  Map<String, dynamic> toJson() {
+    Map<String, dynamic> jsonTest = <String, dynamic>{};
+    jsonTest.addAll({
+      "context": context ?? "",
+      "businessPhones": businessPhones ?? "",
+      "displayName": displayName ?? "",
+      "givenName": givenName ?? "",
+      "jobTitle": jobTitle,
+      "mail": mail ?? "",
+      "mobilePhone": mobilePhone ?? "",
+      "officeLocation": officeLocation ?? "",
+      "preferredLanguage": preferredLanguage,
+      "surname": surname ?? "",
+      "userPrincipalName": userPrincipalName ?? "",
+      "id": id ?? "",
+    });
+
+    return jsonTest;
+  }
+
+  static User fromJson(json) => User(
+        context: json['@odata.context'],
+        businessPhones: json['businessPhones'],
+        displayName: json['displayName'],
+        givenName: json['givenName'],
+        jobTitle: json['jobTitle'],
+        mail: json['mail'],
+        mobilePhone: json['mobilePhone'],
+        officeLocation: json['officeLocation'],
+        preferredLanguage: json['preferredLanguage'],
+        surname: json['surname'],
+        userPrincipalName: json['userPrincipalName'],
+        id: json['id'],
       );
 }

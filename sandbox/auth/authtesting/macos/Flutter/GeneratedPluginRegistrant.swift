7dc04a1dd38c03ecdd906360f9f7d20713a7d5a9
Patrick Grubauer <40146720180276@litec.ac.at>
2022-10-28 09:27:43 +0200

    added .nev

--- sandbox/auth/authtesting/macos/Flutter/GeneratedPluginRegistrant.swift
+++ sandbox/auth/authtesting/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import azure_ad_authentication
+import flutter_secure_storage_macos
+import shared_preferences_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AzureAdAuthenticationPlugin.register(with: registry.registrar(forPlugin: "AzureAdAuthenticationPlugin"))
+  FlutterSecureStorageMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }

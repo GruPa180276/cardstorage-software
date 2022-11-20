4a64265ce7d9938f6a9e92aadcfea0d1605b2203
Benedikt Z <40146720180320@litec.ac.at>
2022-11-20 11:13:20 +0100

    ZoecBe: Added Methodes to Put, Post, Delete and Send Data to API

--- src/admin_login/lib/pages/widget/data.dart
+++ src/admin_login/lib/pages/widget/data.dart
@@ -19,20 +19,72 @@ class Data {
       title: json['name'],
     );
   }
-
-  Data fromJson(json) => Data(
-        id: json['id'],
-        userId: json['id'],
-        title: json['name'],
-      );
 }
 
 Future<List<Data>> fetchData() async {
-  final response = await http.get(Uri.parse('http://10.0.2.2:7171/card'));
+  final response = await http.get(
+    Uri.parse('http://10.0.2.2:7171/card'),
+  );
   if (response.statusCode == 200) {
     List jsonResponse = json.decode(response.body);
     return jsonResponse.map((data) => Data.fromJson(data)).toList();
   } else {
     throw Exception('Unexpected error occured!');
+  }
+}
+
+Future<Data> deleteData(int id) async {
+  final http.Response response = await http.delete(
+    Uri.parse('http://10.0.2.2:7171/card'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  );
+  if (response.statusCode == 200) {
+    return Data.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to delete album.');
+  }
+}
+
+Future<Data> updateData(
+    String title, int id, String imgUrl, int quantity) async {
+  final http.Response response = await http.put(
+    Uri.parse('http://10.0.2.2:7171/card'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'title': title,
+      'id': id.toString(),
+      'imgUrl': imgUrl,
+      'quantity': quantity.toString()
+    }),
+  );
+
+  if (response.statusCode == 200) {
+    return Data.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to update album.');
+  }
+}
+
+Future<Data> sendData(String title, int id, String imgUrl, int quantity) async {
+  final http.Response response = await http.post(
+    Uri.parse('http://10.0.2.2:7171/card'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'title': title,
+      'id': id.toString(),
+      'imgUrl': imgUrl,
+      'quantity': quantity.toString()
+    }),
+  );
+  if (response.statusCode == 201) {
+    return Data.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to load album');
   }
 }

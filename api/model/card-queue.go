bfbed1b098b207b7b1285d054e296c1d03623a45
MayrJo170211 <40146720170211@litec.ac.at>
2022-12-25 18:55:50 +0100

    correctly de/serialize timestamp

--- api/model/card-queue.go
+++ api/model/card-queue.go
@@ -1,14 +1,45 @@
 package model
 
 import (
+	"encoding/json"
 	"time"
 )
 
 type CardQueueEntry struct {
-	Id       int           `json:"id,omitempty"`
-	Userid   int           `json:"user-id"`
-	Since    time.Duration `json:"since"`
-	Until    time.Duration `json:"until"`
-	Returned bool          `json:"returned"`
-	Reserved bool          `json:"reserved"`
+	Id       int       `json:"id,omitempty"`
+	Userid   int       `json:"user-id"`
+	Since    time.Time `json:"since"`
+	Until    time.Time `json:"until"`
+	Returned bool      `json:"returned"`
+	Reserved bool      `json:"reserved"`
+}
+
+func (self *CardQueueEntry) MarshalJSON() ([]byte, error) {
+	type Alias CardQueueEntry
+	return json.Marshal(&struct {
+		Since int64 `json:"since"`
+		Until int64 `json:"until"`
+		*Alias
+	}{
+		Since: self.Since.Unix(),
+		Until: self.Until.Unix(),
+		Alias: (*Alias)(self),
+	})
+}
+
+func (self *CardQueueEntry) UnmarshalJSON(data []byte) error {
+	type Alias CardQueueEntry
+	aux := &struct {
+		Since int64 `json:"since"`
+		Until int64 `json:"until"`
+		*Alias
+	}{
+		Alias: (*Alias)(self),
+	}
+	if err := json.Unmarshal(data, aux); err != nil {
+		return err
+	}
+	self.Since = time.Unix(aux.Since, 0)
+	self.Until = time.Unix(aux.Until, 0)
+	return nil
 }

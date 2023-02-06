301a39a117535602fd2fd908bb46e4f10162ec8d
Benedikt Z <40146720180320@litec.ac.at>
2023-02-06 13:50:45 +0100

    ZoecBe: Added API Authentification

--- src/admin_login/lib/main.dart
+++ src/admin_login/lib/main.dart
@@ -1,13 +1,12 @@
-import 'dart:io';
-
-import 'package:admin_login/config/token_manager.dart';
 import 'package:flutter/material.dart';
 
+import 'dart:io';
 import 'dart:async';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 import 'package:admin_login/routes.dart';
+import 'package:admin_login/config/token_manager.dart';
 import 'package:admin_login/provider/theme/themes.dart';
 import 'package:admin_login/config/theme/app_preference.dart';
 import 'package:admin_login/pages/navigation/bottom_navigation.dart';
